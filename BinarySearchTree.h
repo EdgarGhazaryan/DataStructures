421e59a4101237f3cd438357db80f2a1f37cbafe
van-anan <109599545+van-anan@users.noreply.github.com>
2022-11-21 11:28:44 +0400

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -1,365 +1,256 @@
 #pragma once
 
-#include <stack>
-#include <iostream>
+#include "BinaryTree.h"
+#include "DataStructuresExceptions.h"
+
+#include <algorithm>
+
 template <typename T>
-struct Node {
-    Node(T info, Node<T>* left = nullptr, Node<T>* right = nullptr)
-        : m_info(info), m_left(left), m_right(right) {}
-    T m_info;
-    Node<T>* m_left;
-    Node<T>* m_right;
-};
-template <typename T>
-class Binarytree {
-private:
-    Node<T>* m_root;
+class SearchTree {
 public:
-    Binarytree() : m_root(nullptr) {};
+	SearchTree() : m_root(nullptr), m_size(0) {}
+	SearchTree(const SearchTree<T>&);
+	SearchTree<T>& operator=(const SearchTree<T>&);
+	virtual ~SearchTree();
 
-    ~Binarytree();
+	bool empty() const { return m_size == 0; }
+	int get_size() const { return m_size; }
+	BinaryTree::Node<T>* find(const T&);
+	const BinaryTree::Node<T>* find(const T&) const;
 
-    void insert(T);
+	virtual void insert(const T&);
+	virtual void remove(const T&);
 
-    void remove(T);
-
-    void preorder_traverse_It(void (*function)(T));
-
-    void postorder_traverse_It(void (*function)(T));
-
-    void inorder_traverse_It(void (*function)(T));
-
-    void preorder_traverse(void(*function)(T));
-
-    void postorder_traverse(void (*function)(T));
-
-    void inorder_traverse(void (*function)(T));
-
-    int height();
-
-    int node();
-
-    Node<T>* search(T);
-
-    Node<T>* searchfather(T);
+	const BinaryTree::Node<T>* straight_first() const { return m_root; }
+	void straight_next(std::stack<const BinaryTree::Node<T>*>&) const;
 
 private:
-    int node(Node<T>*);
+	void allocate(const SearchTree<T>&);
+	void allocate_help(BinaryTree::Node<T>*, BinaryTree::Node<T>*);
+	void deallocate(BinaryTree::Node<T>*);
 
-    int height(Node<T>*);
+	BinaryTree::Node<T>* symetric_next(BinaryTree::Node<T>*);
 
-    void destroy(Node<T>*);
+	BinaryTree::Node<T>* find_father(const T&);
+	BinaryTree::Node<T>* child_search(BinaryTree::Node<T>*, const T&);
+	void delete_node(BinaryTree::Node<T>*, BinaryTree::Node<T>*);
 
-    void insert(T, Node<T>*);
-
-    void remove(T, Node<T>*);
-
-    void preorder_traverse_It(Node<T>*, void (*function)(T));
-
-    void postorder_traverse_It(Node<T>*, void (*function)(T));
-
-    void inorder_traverse_It(Node<T>*, void (*function)(T));
-
-    void preorder_traverse(Node<T>*, void (*function)(T));
-
-    void postorder_traverse(Node<T>*, void (*function)(T));
-
-    void inorder_traverse(Node<T>*, void (*function)(T));
-
-    Node<T>* search(T, Node<T>*);
-
-    Node<T>* searchfather(T, Node<T>*);
+protected:
+	BinaryTree::Node<T>* m_root;
+	int m_size;
 };
-template <typename T>
-Binarytree<T>::~Binarytree() { destroy(m_root); }
-template <typename T>
-void Binarytree<T>::inorder_traverse(Node<T>* t, void (*function)(T)) {
-    if (t != nullptr) {
-        inorder_traverse(t->m_left, function);
-        std::cout << t->m_info << ',';
-        inorder_traverse(t->m_right, function);
-    }
-}
-template <typename T>
-void Binarytree<T>::inorder_traverse(void (*function)(T)) {
-    inorder_traverse(m_root,function);
-    std::cout << std::endl;
-}
-template <typename T>
-void Binarytree<T>::preorder_traverse(Node<T>* t, void (*function)(T)) {
-    if (t != nullptr) {
-        function(t->m_info);
-        preorder_traverse(t->m_left,function);
-        preorder_traverse(t->m_right,function);
-    }
-}
-template <typename T>
-void Binarytree<T>::preorder_traverse(void (*function)(T)) {
-    preorder_traverse(m_root,function);
-    std::cout << std::endl;
-}
-template <typename T>
-void Binarytree<T>::postorder_traverse(Node<T>* t, void (*function)(T)) {
-    if (t != nullptr) {
-        postorder_traverse(t->m_left, function);
-        postorder_traverse(t->m_right, function);
-        function(t->m_info);
-    }
-}
-template <typename T>
-void Binarytree<T>::postorder_traverse(void (*function)(T)) {
-    postorder_traverse(m_root, function);
-    std::cout << std::endl;
-}
-template <typename T>
-void Binarytree<T>::insert(T info, Node<T>* t) {
-    if (info < t->m_info) {
-        if (t->m_left != nullptr) {
-            insert(info, t->m_left);
-        }
-        else {
-            t->m_left = new Node<T>(info);
-        }
-    }
-    else {
-        if (t->m_right != nullptr) {
-            insert(info, t->m_right);
-        }
-        else {
-            t->m_right = new Node<T>(info);
-        }
-    }
-}
-template <typename T>
-void Binarytree<T>::insert(T info) {
-    if (m_root == nullptr) {
-        m_root = new Node<T>(info);
-    }
-    else {
-        insert(info, m_root);
-    }
-}
-template <typename T>
-void Binarytree<T>::remove(T element, Node<T>* t) {
-    if (t != nullptr) {
-        Node<T>* ptr1 = searchfather(element);
-        Node<T>* ptr = search(element);
-        if (ptr->m_left == nullptr && ptr->m_right == nullptr) {
-            ptr1->m_left == ptr ? ptr1->m_left = nullptr : ptr1->m_right = nullptr;
-            delete ptr;
-            return;
-        }
-        if (ptr == m_root) {
-            Node<T>* ptr2 = m_root->m_left;
-            if (ptr2) {
-                Node<T>* tmp = nullptr;
-                if (ptr2->m_right == nullptr) {
-                    m_root = ptr2;
-                    m_root->m_right = ptr->m_right;
-                    delete ptr;
-                    return;
-                }
-                while (ptr2->m_right != nullptr) {
-                    tmp = ptr2;
-                    ptr2 = ptr2->m_right;
-                }
-                tmp->m_right = ptr2->m_left;
-            }
-            else {
-                ptr2 = m_root->m_right;
-                if (ptr2) {
-                    Node<T>* tmp = nullptr;
-                    if (ptr2->m_left == nullptr) {
-                        m_root = ptr2;
-                        m_root->m_left = ptr->m_left;
-                        delete ptr;
-                        return;
-                    }
-                    while (ptr2->m_left != nullptr) {
-                        tmp = ptr2;
-                        ptr2 = ptr2->m_left;
-                    }
-                    tmp->m_left = ptr2->m_right;
-                }
-            }
-            ptr2->m_left = ptr->m_left;
-            ptr2->m_right = ptr->m_right;
-            m_root = ptr2;
-            delete ptr;
-            return;
-        }
-        else if (ptr->m_right == nullptr) {
-            ptr1->m_right == ptr ? ptr1->m_right = ptr->m_left : ptr1->m_left = ptr->m_left;
-            delete ptr;
-            return;
-        }
-        else if (ptr->m_left == nullptr) {
-            ptr1->m_left == ptr ? ptr1->m_left = ptr->m_right : ptr1->m_right = ptr->m_right;
-            delete ptr;
-            return;
-        }
-        else {
-            Node<T>* tmp = ptr->m_left;
-            while (tmp->m_right != nullptr) {
-                tmp = tmp->m_right;
-            }
-            if (ptr1->m_left == ptr) {
-                ptr1->m_left = tmp;
-            }
-            else {
-                ptr1->m_right = tmp;
-            }
-            tmp->m_right = ptr->m_right;
-            delete ptr;
-            }
-        }
-   }
 
-template<typename T>
-void Binarytree<T>::remove(T element) {
-    remove(element, m_root);
-}
 template <typename T>
-void Binarytree<T>::destroy(Node<T>* t) {
-    if (t != nullptr) {
-        destroy(t->m_left);
-        destroy(t->m_right);
-        delete t;
-    }
+SearchTree<T>::SearchTree(const SearchTree<T>& object) : m_root(nullptr), m_size(0) {
+	allocate(object);
 }
+
 template <typename T>
-Node<T>* Binarytree<T>::search(T info) {
-    return search(info, m_root);
+SearchTree<T>& SearchTree<T>::operator=(const SearchTree<T>& object) {
+	if (this != &object) {
+		m_size = 0;
+		deallocate(m_root);
+		allocate(object);
+	}
+	return *this;
 }
+
 template <typename T>
-Node<T>* Binarytree<T>::search(T info, Node<T>* t) {
-    if (t != nullptr) {
-        if (t->m_info == info) {
-            return t;
-        }
-        else if (t->m_info > info) {
-            return search(info, t->m_left);
-        }
-        else {
-            return search(info, t->m_right);
-        }
-    }
-    return nullptr;
+SearchTree<T>::~SearchTree() {
+	deallocate(m_root);
 }
+
 template <typename T>
-Node<T>* Binarytree<T>::searchfather(T info) {
-    return searchfather(info, m_root);
+BinaryTree::Node<T>* SearchTree<T>::find(const T& element) {
+	if (m_root == nullptr) {
+		return nullptr;
+	}
+	BinaryTree::Node<T>* ptr = m_root;
+	while (ptr != nullptr && ptr->m_info != element) {
+		if (element < ptr->m_info) {
+			ptr = ptr->m_left;
+		}
+		else {
+			ptr = ptr->m_right;
+		}
+	}
+	return ptr;
 }
+
 template <typename T>
-Node<T>* Binarytree<T>::searchfather(T info, Node<T>* t) {
-    if (t != nullptr) {
-        if (info == t->m_info) {
-            return nullptr;
-        }
-        Node<T>* ptr = search(info);
-        if (t->m_left == ptr || t->m_right == ptr) {
-            return t;
-        }
-        else if (t->m_info > info) {
-            return searchfather(info, t->m_left);
-        }
-        else {
-            return searchfather(info, t->m_right);
-        }
-    }
-    return nullptr;
+const BinaryTree::Node<T>* SearchTree<T>::find(const T& element) const {
+	if (m_root == nullptr) {
+		return nullptr;
+	}
+	const BinaryTree::Node<T>* ptr = m_root;
+	while (ptr != nullptr && ptr->m_info != element) {
+		if (element < ptr->m_info) {
+			ptr = ptr->m_left;
+		}
+		else {
+			ptr = ptr->m_right;
+		}
+	}
+	return ptr;
 }
+
 template <typename T>
-int Binarytree<T>::node() {
-    return node(m_root);
+void SearchTree<T>::insert(const T& element) {
+	BinaryTree::Node<T>* tmp = m_root, * tmp_parent{};
+	while (tmp != nullptr) {
+		tmp_parent = tmp;
+		tmp = (element < tmp->m_info) ? tmp->m_left : tmp->m_right;
+	}
+	if (tmp_parent == nullptr) {
+		m_root = new BinaryTree::Node<T>(element);
+	}
+	else {
+		if (element < tmp_parent->m_info) {
+			tmp_parent->m_left = new BinaryTree::Node<T>(element);
+		}
+		else {
+			tmp_parent->m_right = new BinaryTree::Node<T>(element);
+		}
+	}
+	++m_size;
 }
+
 template <typename T>
-int Binarytree<T>::node(Node<T>* t) {
-    if (t == nullptr) {return 0;}
-    return node(t->m_left) + node(t->m_right) + 1;
+void SearchTree<T>::remove(const T& element) {
+	BinaryTree::Node<T>* father = find_father(element);
+	BinaryTree::Node<T>* child = child_search(father, element);
+	if (child == nullptr) {
+		throw IsEmptyException;
+	}
+	if (child->m_left != nullptr && child->m_right != nullptr) {
+		father = symetric_next(child);
+		child = child_search(father, element);
+	}
+	delete_node(father, child);
+	--m_size;
 }
+
 template <typename T>
-int Binarytree<T>::height() {
-    return height(m_root);
+void SearchTree<T>::straight_next(std::stack<const BinaryTree::Node<T>*>& s) const {
+	const BinaryTree::Node<T>* tmp = s.top();
+	s.pop();
+	if (tmp->m_right != nullptr) {
+		s.push(tmp->m_right);
+	}
+	if (tmp->m_left != nullptr) {
+		s.push(tmp->m_left);
+	}
 }
+
 template <typename T>
-int Binarytree<T>::height(Node<T>* t) {
-    if ((t == nullptr)||( t->m_left == nullptr && t->m_right == nullptr)){
-        return 0;
-    }
-    int left = height(t->m_left);
-    int right = height(t->m_right);
-    if (left > right) {
-        return left + 1;
-    }
-    else {
-        return right + 1;
-    }
+void SearchTree<T>::allocate(const SearchTree<T>& object) {
+	if (!object.empty()) {
+		m_root = new BinaryTree::Node<T>(object.m_root->m_info);
+		allocate_help(m_root, object.m_root);
+	}
 }
+
 template <typename T>
-void Binarytree<T>::preorder_traverse_It(void (*function)(T)) {
-    preorder_traverse_It(m_root, function);
+void SearchTree<T>::allocate_help(BinaryTree::Node<T>* real, BinaryTree::Node<T>* object) {
+	if (object->m_left != nullptr) {
+		real->m_left = new BinaryTree::Node<T>(object->m_left->m_info);
+		allocate_help(real->m_left, object->m_left);
+	}
+	if (object->m_right != nullptr) {
+		real->m_right = new BinaryTree::Node<T>(object->m_right->m_info);
+		allocate_help(real->m_right, object->m_right);
+	}
 }
+
 template <typename T>
-void Binarytree<T>::preorder_traverse_It(Node<T>* t, void (*function)(T)){
-    Node<T>* ptr = t;
-    std::stack<Node<T>*> s;
-    while (true) {
-        while (ptr != nullptr) {
-            function(ptr->m_info);
-            if (ptr->m_right != nullptr) {
-                s.push(ptr->m_right);
-            }
-            ptr = ptr->m_left;
-        }
-        if (s.empty()) { break; }
-            ptr = s.top();
-            s.pop();
-    }
+void SearchTree<T>::deallocate(BinaryTree::Node<T>* root) {
+	if (root == nullptr) {
+		return;
+	}
+	BinaryTree::Node<T>* left = root->m_left;
+	BinaryTree::Node<T>* right = root->m_right;
+	delete root;
+	if (left != nullptr) {
+		deallocate(left);
+	}
+	if (right != nullptr) {
+		deallocate(right);
+	}
 }
+
 template <typename T>
-void Binarytree<T>::inorder_traverse_It(void (*function)(T)) {
-    inorder_traverse_It(m_root, function);
+BinaryTree::Node<T>* SearchTree<T>::symetric_next(BinaryTree::Node<T>* current) {
+	BinaryTree::Node<T>* to_remove = current;
+	BinaryTree::Node<T>* curr_father = current;
+	current = current->m_right;
+	while (current->m_left != nullptr) {
+		curr_father = current;
+		current = current->m_left;
+	}
+	std::swap(current->m_info, to_remove->m_info);
+	return curr_father;
 }
+
 template <typename T>
-void Binarytree<T>::inorder_traverse_It(Node<T>* t, void (*function)(T)) {
-    Node<T>* ptr = t;
-    std::stack<Node<T>*> s;
-    while (true) {
-        while (ptr != nullptr) {
-            s.push(ptr);
-            ptr = ptr->m_left;
-        }
-        if (s.empty()) { break; }
-        ptr = s.top();
-        s.pop();
-        function(ptr->m_info);
-        ptr = ptr->m_right;
-    }
+BinaryTree::Node<T>* SearchTree<T>::find_father(const T& element) {
+	if (m_root == nullptr) {
+		return nullptr;
+	}
+	BinaryTree::Node<T>* pointer = m_root;
+	BinaryTree::Node<T>* pointer1 = m_root;
+	while (pointer != nullptr && pointer->m_info != element) {
+		pointer1 = pointer;
+		if (pointer->m_info > element) {
+			pointer = pointer->m_left;
+		}
+		else {
+			pointer = pointer->m_right;
+		}
+	}
+	return (pointer == nullptr) ? nullptr : pointer1;
 }
+
 template <typename T>
-void Binarytree<T>::postorder_traverse_It(void (*function)(T)) {
-    postorder_traverse_It(m_root, function);
+BinaryTree::Node<T>* SearchTree<T>::child_search(BinaryTree::Node<T>* father, const T& element) {
+	if (father == nullptr) { 
+		return nullptr; 
+	}
+	if (father == m_root && ((father->m_left == nullptr && father->m_right == nullptr) || father->m_info == element)) {
+		return father;
+	}
+	if (father->m_left != nullptr && father->m_left->m_info == element) { 
+		return father->m_left;
+	}
+	return father->m_right;
 }
+
 template <typename T>
-void Binarytree<T>::postorder_traverse_It(Node<T>* t, void (*function)(T)) {
-    Node<T>* ptr = t;
-    std::stack<Node<T>*> s;
-    while (true) {
-        while (ptr != nullptr ) {
-            s.push(ptr);
-            s.push(ptr);
-            ptr = ptr->m_left;
-        }
-        if (s.empty()) { break; }
-        ptr = s.top();
-        s.pop();
-        if (!s.empty() && s.top() == ptr) {
-            ptr = ptr->m_right;
-        }
-        else {
-            function(ptr->m_info);
-            ptr = nullptr;
-        }
-    }
+void SearchTree<T>::delete_node(BinaryTree::Node<T>* father, BinaryTree::Node<T>* child) {
+	if (child->m_left == nullptr && child->m_right == nullptr) {
+		if (child == m_root) {
+			m_root = nullptr;
+			delete child;
+			return;
+		}
+		if (child == father->m_left) {
+			father->m_left = nullptr;
+		}
+		if (child == father->m_right) {
+			father->m_right = nullptr;
+		}
+	}
+	else {
+		if (child == m_root) {
+			m_root = (m_root->m_left != nullptr) ? m_root->m_left : m_root->m_right;
+			delete child;
+			return;
+		}
+		if (father->m_left == child) {
+			father->m_left = (child->m_left != nullptr) ? child->m_left : child->m_right;
+		}
+		if (father->m_right == child) {
+			father->m_right = (child->m_left != nullptr) ? child->m_left : child->m_right;
+		}
+	}
+	delete child;
 }
